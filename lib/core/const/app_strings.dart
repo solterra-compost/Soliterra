0b9786765be4303a5a43b09eedad29cd408cb1be
Harsh Rajput <dev.harsh9730@gmail.com>
2026-05-05 23:52:30 +0530

    fix image extension

--- lib/core/const/app_strings.dart
+++ lib/core/const/app_strings.dart
@@ -1,13 +1,13 @@
 class AppStrings {
-  //App name
+  // App name
   static const String appNameCap = "SOLTERRA";
   static const String appNameSml = "Solterra";
 
-  //App titles
+  // App titles
   static const String appTitle =
       "Regenerating the Earth with Nano-Biotechnology";
 
-  //App Image String
-  static const String imgThumbnel = "assets/images/intro.png";
+  // App Image String
+  static const String imgThumbnel = "assets/images/intro.jpeg";
   static const String imgForm = "assets/images/image.png";
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:solterra/core/router/app_router.dart';
 import 'package:solterra/core/theme/app_theme.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() {
   runApp(const MyApp());
+  usePathUrlStrategy();
 }
 
 class MyApp extends StatelessWidget {
